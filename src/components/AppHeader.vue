9ba9417791b0c6cee579af66f482148b1b4672c8
Tony <tonyguo0815>
2022-05-03 19:29:22 +0800

    version 0.1.0

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <CHeader position="sticky" class="mb-4">
+    <CContainer fluid>
+      <CHeaderToggler class="ps-1" @click="$store.commit('toggleSidebar')">
+        <CIcon icon="cil-menu" size="lg" />
+      </CHeaderToggler>
+      <CHeaderBrand class="mx-auto d-lg-none" to="/">
+        <CIcon :icon="logo" height="48" alt="Logo" />
+      </CHeaderBrand>
+      <CHeaderNav>
+        <AppHeaderDropdownAccnt />
+      </CHeaderNav>
+    </CContainer>
+    <CHeaderDivider />
+    <CContainer fluid>
+      <AppBreadcrumb />
+    </CContainer>
+  </CHeader>
+</template>
+
+<script>
+import AppBreadcrumb from '@/components/AppBreadcrumb.vue'
+import AppHeaderDropdownAccnt from '@/components/AppHeaderDropdownAccnt.vue'
+import { logo } from '@/assets/brand/logo'
+export default {
+  name: 'AppHeader',
+  components: {
+    AppBreadcrumb,
+    AppHeaderDropdownAccnt,
+  },
+  setup() {
+    return {
+      logo,
+    }
+  },
+}
+</script>
---
+++ src/components/AppHeaderDropdownAccnt.vue
@@ -0,0 +1,23 @@
+<template>
+  <CDropdown variant="nav-item">
+    <CDropdownToggle placement="bottom-end" class="py-0" :caret="false">
+      <CAvatar :src="avatar" size="md" />
+    </CDropdownToggle>
+    <CDropdownMenu class="pt-0">
+      <CDropdownItem> <CIcon icon="cil-lock-locked" /> Logout </CDropdownItem>
+    </CDropdownMenu>
+  </CDropdown>
+</template>
+
+<script>
+import avatar from '@/assets/images/avatars/user.jpg'
+export default {
+  name: 'AppHeaderDropdownAccnt',
+  setup() {
+    return {
+      avatar: avatar,
+      itemsCount: 42,
+    }
+  },
+}
+</script>
